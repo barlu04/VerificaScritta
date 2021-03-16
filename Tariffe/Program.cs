a1d03c269fe8a804833b973dc98267ad4bc2a16f
mattiarossi578@gmail.com <mattiarossi578@gmail.com>
2021-03-16 13:11:54 +0100

    Aggiungere i file di progetto.

---
+++ Tariffe/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Tariffe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ VerificaScritta/UnitTest1.cs
@@ -0,0 +1,17 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace VerificaScritta
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            double a = 79.99;
+            double b = 99.99;
+            double c = 115.99;
+
+        }
+    }
+}
